cfd319baa23bf7b27c74365492ca06b0d3a6cafc
PolaAstur <125497558+PolaAstur@users.noreply.github.com>
2023-02-17 21:36:14 +0100

    Add files via upload

---
+++ CarreraSanFermin.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CarreraSanFermin {
+
+	public static void main(String[] args) {
+		int[] arrayVelocidades;
+		int numToros = 1, velocidadDelToro = 0, maximaVelocidad = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime el número de toros : ");
+		arrayVelocidades = new int[sc.nextInt()];
+		//Rellenamos las velocidades de los toros
+		for (int i=0; i < arrayVelocidades.length; i++) {
+			System.out.println("Dame la velocidad del toro : ");
+			arrayVelocidades[i] = sc.nextInt();
+		}
+		Arrays.sort(arrayVelocidades);
+		System.out.println(arrayVelocidades[arrayVelocidades.length-1]);
+	}
+}
